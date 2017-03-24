1e5e32226aba2650f94dbd502a8d472e2f74f333
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-24 17:11:47 +0100

    make the page somewhat responsive, FIXME: Have to reload page for breakpoints to work.

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,6 @@
 export class ItemConfig {
   currencySymbol = '€';
   imagePath = '../assets/images/';
+//imagePath = '../nikolaus-schlifke/angularcodereview/assets/images/';
   expensivePrice = 200;
 }

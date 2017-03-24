cfda8475405e083b728a94d81e06fff43b0be40c
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-24 10:45:32 +0100

    add firebase with mockup catalog

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBuBrz1jZ4wSSIQlsjhmtCo70JPsohKO98',
+  authDomain: 'productcatalog-5677b.firebaseapp.com',
+  databaseURL: 'https://productcatalog-5677b.firebaseio.com',
+  storageBucket: 'productcatalog-5677b.appspot.com',
+  messagingSenderId: '7885215042'
+};
+
